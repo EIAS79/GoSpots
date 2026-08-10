8f38daa63a0fb05595cc366b1a6e3c972f37dfe3
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-10 18:51:25 +0200

    Wire fiscal connector implementations

--- apps/api/src/modules/compliance/compliance.module.ts
+++ apps/api/src/modules/compliance/compliance.module.ts
@@ -4,6 +4,10 @@ import { ComplianceProfileService } from './compliance-profile.service';
 import { ComplianceSecretCryptoService } from './compliance-secret.crypto';
 import { ComplianceService } from './compliance.service';
 import { FiscalDocumentService } from './fiscal-document.service';
+import { FiscalConnectorRegistry } from './fiscal/fiscal-connector.registry';
+import { FiscalizationService } from './fiscal/fiscalization.service';
+import { HttpFiscalConnector } from './fiscal/http-fiscal.connector';
+import { SimulatedFiscalConnector } from './fiscal/simulated-fiscal.connector';
 import { Fa3BuilderService } from './ksef/fa3-builder.service';
 import { KsefClientService } from './ksef/ksef-client.service';
 import { KsefCryptoService } from './ksef/ksef-crypto.service';
@@ -16,6 +20,10 @@ import { PolandComplianceAdapter } from './poland-compliance.adapter';
     ComplianceProfileService,
     ComplianceSecretCryptoService,
     FiscalDocumentService,
+    FiscalizationService,
+    FiscalConnectorRegistry,
+    SimulatedFiscalConnector,
+    HttpFiscalConnector,
     PolandComplianceAdapter,
     Fa3BuilderService,
     KsefCryptoService,
@@ -25,6 +33,7 @@ import { PolandComplianceAdapter } from './poland-compliance.adapter';
     ComplianceService,
     ComplianceProfileService,
     FiscalDocumentService,
+    FiscalizationService,
     PolandComplianceAdapter,
   ],
 })
