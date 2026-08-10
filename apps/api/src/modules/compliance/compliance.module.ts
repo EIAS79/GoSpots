072c44e94354a40628b7975b58509e8625380b1a
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-10 18:32:31 +0200

    Wire complete Chunk 08 compliance services

--- apps/api/src/modules/compliance/compliance.module.ts
+++ apps/api/src/modules/compliance/compliance.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ComplianceController } from './compliance.controller';
+import { ComplianceProfileService } from './compliance-profile.service';
+import { ComplianceSecretCryptoService } from './compliance-secret.crypto';
 import { ComplianceService } from './compliance.service';
+import { FiscalDocumentService } from './fiscal-document.service';
+import { Fa3BuilderService } from './ksef/fa3-builder.service';
 import { KsefClientService } from './ksef/ksef-client.service';
 import { KsefCryptoService } from './ksef/ksef-crypto.service';
 import { PolandComplianceAdapter } from './poland-compliance.adapter';
@@ -9,10 +13,19 @@ import { PolandComplianceAdapter } from './poland-compliance.adapter';
   controllers: [ComplianceController],
   providers: [
     ComplianceService,
+    ComplianceProfileService,
+    ComplianceSecretCryptoService,
+    FiscalDocumentService,
     PolandComplianceAdapter,
+    Fa3BuilderService,
     KsefCryptoService,
     KsefClientService,
   ],
-  exports: [ComplianceService, PolandComplianceAdapter],
+  exports: [
+    ComplianceService,
+    ComplianceProfileService,
+    FiscalDocumentService,
+    PolandComplianceAdapter,
+  ],
 })
 export class ComplianceModule {}
