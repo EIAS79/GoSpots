6e4b740fcc4511a1d9a654cb571a9f9a64aca4da
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-11 12:34:42 +0200

    feat(growth): wire public reservation deposit checkout

--- apps/api/src/modules/growth/growth.module.ts
+++ apps/api/src/modules/growth/growth.module.ts
@@ -5,14 +5,22 @@ import { EventsGrowthService } from './events-growth.service';
 import { GrowthAnalyticsService } from './growth-analytics.service';
 import { GrowthCapacityService } from './growth-capacity.service';
 import { GrowthCrmService } from './growth-crm.service';
+import { GrowthDepositPublicController } from './growth-deposit-public.controller';
+import { GrowthDepositWebhookController } from './growth-deposit-webhook.controller';
 import { GrowthPricingService } from './growth-pricing.service';
 import { GrowthPublicController } from './growth-public.controller';
+import { GrowthPublicDepositService } from './growth-public-deposit.service';
 import { GrowthController } from './growth.controller';
 import { ReservationGrowthService } from './reservation-growth.service';
 
 @Module({
   imports: [GuestCheckModule],
-  controllers: [GrowthController, GrowthPublicController],
+  controllers: [
+    GrowthController,
+    GrowthPublicController,
+    GrowthDepositPublicController,
+    GrowthDepositWebhookController,
+  ],
   providers: [
     ReservationGrowthService,
     GrowthCapacityService,
@@ -21,6 +29,7 @@ import { ReservationGrowthService } from './reservation-growth.service';
     CommerceGrowthService,
     EventsGrowthService,
     GrowthAnalyticsService,
+    GrowthPublicDepositService,
   ],
   exports: [
     ReservationGrowthService,
