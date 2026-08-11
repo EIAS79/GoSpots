967361797467bf4923a674f8a503d48c6960514a
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-11 09:56:55 +0200

    feat(growth): wire capacity and public growth controllers

--- apps/api/src/modules/growth/growth.module.ts
+++ apps/api/src/modules/growth/growth.module.ts
@@ -2,8 +2,26 @@ import { Module } from '@nestjs/common';
 import { CommerceGrowthService } from './commerce-growth.service';
 import { EventsGrowthService } from './events-growth.service';
 import { GrowthAnalyticsService } from './growth-analytics.service';
+import { GrowthCapacityService } from './growth-capacity.service';
+import { GrowthPublicController } from './growth-public.controller';
 import { GrowthController } from './growth.controller';
 import { ReservationGrowthService } from './reservation-growth.service';
 
-@Module({controllers:[GrowthController],providers:[ReservationGrowthService,CommerceGrowthService,EventsGrowthService,GrowthAnalyticsService],exports:[ReservationGrowthService,CommerceGrowthService,EventsGrowthService,GrowthAnalyticsService]})
+@Module({
+  controllers: [GrowthController, GrowthPublicController],
+  providers: [
+    ReservationGrowthService,
+    GrowthCapacityService,
+    CommerceGrowthService,
+    EventsGrowthService,
+    GrowthAnalyticsService,
+  ],
+  exports: [
+    ReservationGrowthService,
+    GrowthCapacityService,
+    CommerceGrowthService,
+    EventsGrowthService,
+    GrowthAnalyticsService,
+  ],
+})
 export class GrowthModule {}
