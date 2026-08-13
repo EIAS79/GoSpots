69d0eb4dce63d50da1e20e93f1ddbd439531beac
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-13 03:24:52 +0200

    feat(api): export feature flag guard from foundation

--- apps/api/src/modules/foundation/foundation.module.ts
+++ apps/api/src/modules/foundation/foundation.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { DomainEventOutboxService } from './domain-event-outbox.service';
+import { FeatureFlagGuard } from './feature-flag.guard';
 import { FeatureFlagService } from './feature-flag.service';
 
 @Global()
 @Module({
-  providers: [DomainEventOutboxService, FeatureFlagService],
-  exports: [DomainEventOutboxService, FeatureFlagService],
+  providers: [DomainEventOutboxService, FeatureFlagService, FeatureFlagGuard],
+  exports: [DomainEventOutboxService, FeatureFlagService, FeatureFlagGuard],
 })
 export class FoundationModule {}
