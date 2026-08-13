bdcabd1c98864f1a11c93ecd7018837540c79ec8
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-13 10:02:39 +0200

    feat(api): export canonical capability service

--- apps/api/src/modules/foundation/foundation.module.ts
+++ apps/api/src/modules/foundation/foundation.module.ts
@@ -1,11 +1,22 @@
 import { Global, Module } from '@nestjs/common';
+import { CapabilityService } from './capability.service';
 import { DomainEventOutboxService } from './domain-event-outbox.service';
 import { FeatureFlagGuard } from './feature-flag.guard';
 import { FeatureFlagService } from './feature-flag.service';
 
 @Global()
 @Module({
-  providers: [DomainEventOutboxService, FeatureFlagService, FeatureFlagGuard],
-  exports: [DomainEventOutboxService, FeatureFlagService, FeatureFlagGuard],
+  providers: [
+    DomainEventOutboxService,
+    FeatureFlagService,
+    CapabilityService,
+    FeatureFlagGuard,
+  ],
+  exports: [
+    DomainEventOutboxService,
+    FeatureFlagService,
+    CapabilityService,
+    FeatureFlagGuard,
+  ],
 })
 export class FoundationModule {}
