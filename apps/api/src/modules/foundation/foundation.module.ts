a3d2a99ab6b3c9c438a469216188d2618b3115b2
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-13 10:05:48 +0200

    feat(api): export domain event consumer mechanics

--- apps/api/src/modules/foundation/foundation.module.ts
+++ apps/api/src/modules/foundation/foundation.module.ts
@@ -1,5 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { CapabilityService } from './capability.service';
+import { DomainEventConsumerService } from './domain-event-consumer.service';
 import { DomainEventOutboxService } from './domain-event-outbox.service';
 import { FeatureFlagGuard } from './feature-flag.guard';
 import { FeatureFlagService } from './feature-flag.service';
@@ -8,12 +9,14 @@ import { FeatureFlagService } from './feature-flag.service';
 @Module({
   providers: [
     DomainEventOutboxService,
+    DomainEventConsumerService,
     FeatureFlagService,
     CapabilityService,
     FeatureFlagGuard,
   ],
   exports: [
     DomainEventOutboxService,
+    DomainEventConsumerService,
     FeatureFlagService,
     CapabilityService,
     FeatureFlagGuard,
