891e750b98fd9b68e332618627c90ff1fcaa3a7e
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-18 11:59:48 +0200

    phase10: wire accountability service and interceptor

--- apps/api/src/modules/workforce/workforce.module.ts
+++ apps/api/src/modules/workforce/workforce.module.ts
@@ -1,1 +1,25 @@
-import { Module } from '@nestjs/common'; import { PrismaModule } from '../../prisma/prisma.module'; import { AuditModule } from '../audit/audit.module'; import { WorkforceController } from './workforce.controller'; import { WorkforceService } from './workforce.service'; @Module({imports:[PrismaModule,AuditModule],controllers:[WorkforceController],providers:[WorkforceService],exports:[WorkforceService]}) export class WorkforceModule {}
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { AuditModule } from '../audit/audit.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { Phase10AccountabilityController } from './phase10-accountability.controller';
+import { Phase10AccountabilityInterceptor } from './phase10-accountability.interceptor';
+import { Phase10AccountabilityService } from './phase10-accountability.service';
+import { WorkforceController } from './workforce.controller';
+import { WorkforceService } from './workforce.service';
+
+@Module({
+  imports: [PrismaModule, AuditModule, NotificationsModule],
+  controllers: [WorkforceController, Phase10AccountabilityController],
+  providers: [
+    WorkforceService,
+    Phase10AccountabilityService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: Phase10AccountabilityInterceptor,
+    },
+  ],
+  exports: [WorkforceService, Phase10AccountabilityService],
+})
+export class WorkforceModule {}
