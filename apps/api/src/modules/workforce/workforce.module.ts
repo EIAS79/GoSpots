23456b97cf91bbbab4c4d0f96dadb1aed364a65c
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-18 14:25:34 +0200

    phase10: register performance projection

--- apps/api/src/modules/workforce/workforce.module.ts
+++ apps/api/src/modules/workforce/workforce.module.ts
@@ -7,6 +7,7 @@ import { Phase10AccountabilityController } from './phase10-accountability.contro
 import { Phase10AccountabilityInterceptor } from './phase10-accountability.interceptor';
 import { Phase10AccountabilityService } from './phase10-accountability.service';
 import { Phase10ClockInRestrictionInterceptor } from './phase10-clockin-restriction.interceptor';
+import { Phase10PerformanceService } from './phase10-performance.service';
 import { WorkforceController } from './workforce.controller';
 import { WorkforceService } from './workforce.service';
 
@@ -16,6 +17,7 @@ import { WorkforceService } from './workforce.service';
   providers: [
     WorkforceService,
     Phase10AccountabilityService,
+    Phase10PerformanceService,
     {
       provide: APP_INTERCEPTOR,
       useClass: Phase10ClockInRestrictionInterceptor,
@@ -25,6 +27,6 @@ import { WorkforceService } from './workforce.service';
       useClass: Phase10AccountabilityInterceptor,
     },
   ],
-  exports: [WorkforceService, Phase10AccountabilityService],
+  exports: [WorkforceService, Phase10AccountabilityService, Phase10PerformanceService],
 })
 export class WorkforceModule {}
